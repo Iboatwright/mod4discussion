d68eeb0c1e447849ac7f29a2afa66fa45e3770e8
Ivan Boatwright <iboatwright@users.noreply.github.com>
2016-02-24 06:46:11 -0500

    most of main is added.  only fluffy is defined.  Want to add a control loop to main, notes under todo.

--- tip_tax_total_enhanced.py
+++ tip_tax_total_enhanced.py
@@ -8,3 +8,47 @@
 # February 23, 2016
 
 # Global Constants
+SALES_TAX = .06
+# baseTips is a Dictionary with the starting price of each range as the key
+#   and the tip percent as a float as the value.
+BASE_TIPS = {.01: 0.1, 6: 0.13, 12.01: 0.16, 17.01: 0.19,
+                       25.01: .22}
+
+def main():
+    # Local variables
+    mealPrice = 0.0
+    mealTax = 0.0
+    mealTip = 0.0
+    mealTotal = 0.0
+    # List used for user prompt and input validation.
+    #   The first value is the expected input and second is the prompt.
+    requestList = [['float', 'price of the meal']]
+
+    # Display intro to user.
+    fluffy_intro()
+
+    # Control loop for main()
+        #todo: add a while loop and a menu for 1) calculate another meal,
+        #todo:   0) exit
+    # Assigns user entered meal price after validating entered value.
+    mealPrice = get_valid_inputs(requestList)
+
+    # Assigns meal Tax, Tip and Total to their respective variables
+    mealTax, mealTip, mealTotal = calc_meal(mealPrice)
+
+    # Display the costs of the meal to the user.
+    display_results(mealPrice, mealTax, mealTip, mealTotal)
+
+    # End of main()
+    return None
+
+
+
+# Displays an introduction to the program and describes what it does.
+def fluffy_intro():
+    print('Welcome to the Tip, Tax, Total Enhanced program.'
+          'This program requests the price of a meal and then calculates'
+          'the meal tip, meal tax, and total cost of the meal.'
+          'It then displays the results.')
+
+#todo: add the rest of the functions.
