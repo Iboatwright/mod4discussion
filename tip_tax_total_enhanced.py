9329c8d190810a0444f4e70fb00d1294f68d10ae
Ivan Boatwright <iboatwright@users.noreply.github.com>
2016-02-24 06:31:32 -0500

    Forgot to create the meal and tip calculators.

--- tip_tax_total_enhanced.py
+++ tip_tax_total_enhanced.py
@@ -1,8 +1,8 @@
 # tip_tax_total_enhanced.py
 # Exercises selected: Lab 4.6 - Programming Challenge 1 - Tip, Tax, and Total
 # Name of program: T3E, Tip Tax Total Enhanced
-# Description of program: This program will ask the user to input the
-# Finally it will display the results to the user.
+# Description of program: This program will ask the user to input a meal price
+# and displays the cost breakdown of the meal price, tip, tax and the total.
 #
 # Ivan Boatwright
 # February 23, 2016
