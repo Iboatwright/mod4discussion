b8efb143e4d60ae34427dc1c04af739f0206dc73
Ivan Boatwright <iboatwright@users.noreply.github.com>
2016-02-27 21:19:48 -0500

    Added a test for negative numbers and fixed some comments.

--- tip_tax_total_enhanced.py
+++ tip_tax_total_enhanced.py
@@ -1,16 +1,16 @@
 # tip_tax_total_enhanced.py
 # Exercises selected: Lab 4.6 - Programming Challenge 1 - Tip, Tax, and Total
-# Name of program: T3E, Tip Tax Total Enhanced
+# Name of program: Tip Tax Total Enhanced
 # Description of program: This program will ask the user to input a meal price
 # and displays the cost breakdown of the meal price, tip, tax and the total.
 #
 # Ivan Boatwright
-# February 23, 2016
+# February 27, 2016
 
 # Global Constants
 SALES_TAX = .06
-# baseTips is a Dictionary with the starting price of each range as the key
-#   and the tip percent as a float as the value.
+# BASE_TIPS is a Constant Dictionary with the starting price of each range
+#   as the key and the tip percent as a float as the value.
 BASE_TIPS = {.01: 0.1, 6: 0.13, 12.01: 0.16, 17.01: 0.19,
                        25.01: .22}
 
@@ -22,9 +22,9 @@ def main():
                  'total': 0.0}
     # List Constant used for user prompt and input validation.
     #   The first value is the expected input and second is the prompt.
-    REQUEST_LIST = [['float', 'price of the meal']]
+    REQUEST_LIST = [['float and nonnegative', 'price of the meal']]
 
-    #  intro to user.
+    # Displays the intro to user.
     fluffy_intro()
 
     # Assigns user entered meal price after validating entered value.
@@ -34,7 +34,7 @@ def main():
     #   to their respective variables in the mealCosts Dict.
     calc_meal(mealCosts)
 
-    #  the costs of the meal to the user.
+    # Displays the costs of the meal to the user.
     display_results(mealCosts)
 
     # End of main()
@@ -42,12 +42,12 @@ def main():
 
 
 
-# s an introduction to the program and describes what it does.
+# Displays an introduction to the program and describes what it does.
 def fluffy_intro():
-    print('Welcome to the Tip, Tax, Total Enhanced program.'
-          'This program requests the price of a meal and then calculates'
-          'the meal tip, meal tax, and total cost of the meal.'
-          'It then displays the results.')
+    print('\nWelcome to the Tip, Tax, Total Enhanced program.\n'
+          'This program requests the price of a meal and then calculates\n'
+          'the meal tip, meal tax, and total cost of the meal.\n'
+          'It then displays the results.\n')
 
 
 # get_valid_inputs requests input from the user then tests the input.
@@ -62,13 +62,13 @@ def get_valid_inputs(requestsList):
     #  to request.
     for request in requestsList:
         # untestedInput is a holding variable for testing user input validity.
-        # First user prompt before testing loop
+        # First user prompt before testing loop.
         untestedInput = prompt_user_for_input(request[1])
 
         # If test_value returns True, Not converts it to False and the While
-        #  Loop will not execute.
+        #   Loop will not execute.
         # If test_value returns False, the While executes and the user is
-        #  prompted to enter a valid value.
+        #   prompted to enter a valid value.
         while (not test_value(request[0], untestedInput)):
 
             print('!!! Error: {} is not a valid value.'.format(untestedInput))
@@ -95,14 +95,15 @@ def prompt_user_for_input(promptTerm):
 # It returns True or False to the calling Module.
 def test_value(testCondition, testItem):
     # The If-Then-Else structure functions as a Switch for test selection.
-    if testCondition == 'float':
-        # Try will try to convert testItem to a float. If it succeeds
-        #  True is returned to the calling module.  If float(testItem)
-        #  creates an error, except will stop the error from writing to
-        #  the screen and returns False.
+    if testCondition == 'float and nonnegative':
+        # If float(testItem) is greater than or equal to zero True is
+        #  returned.  If float(testItem) creates an error or is less than
+        #  zero False is returned.
         try:
-           float(testItem)
-           return True
+            if float(testItem) >= 0:
+                return True
+            else:
+                return False
         except:
             return False
     else:
